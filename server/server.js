058dc3c782d77eace7f502be5a2a2d5a12e6d91d
vasundhra gupta <vasundhragupta962@gmail.com>
2025-04-06 11:05:31 +0530

    ready fr deployment

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ import { connectDB } from "./src/db/connection.js";
 const port = process.env.PORT || 4100;
 app.listen(port, () => {
     console.log("server is listening at port ", port);
-    // console.log("connecting to database...");
+    console.log("connecting to database...");
 });
-// connectDB();
+connectDB();
